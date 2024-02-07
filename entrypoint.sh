2c6ec8d55d9d84f39d9eec5c84736b7f05d82dd8
noemi777 <ada_hack505@womenintech.mx>
2024-02-07 15:04:57 -0600

    Actualización entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,12 @@
 
 cd /app
 
-python manage.py collectstatic --clear --noinput
-
+$ python manage.py collectstatic --noinput --verbose
+#para recopilar todos los archivos estáticos de todas las aplicaciones del proyecto
+# y almacenarlos en un único directorio
+#Identificar errores o problemas que impidan recojer los archivo estaticos
 python manage.py migrate
 
 python manage.py runserver 0.0.0.0:8000
 
-exec "$@"
+exec "$@" #ejecutará la orden dada por los parámetros
