b55f021c306cf545dee4f2a9d78a98c430913a0f
Fernando Gallardo <fernando@hackademy.mx>
2024-02-06 22:14:07 -0700

    Orden cors headers
    Cambio del orden

--- inventario/settings.py
+++ inventario/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', #corsheaders
+    'django.middleware.common.CommonMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,8 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware', #corsheaders
-    'django.middleware.common.CommonMiddleware' 
+    
 ]
 
 CORS_ALLOWED_ORIGINS = [
